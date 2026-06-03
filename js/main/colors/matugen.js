a1a8a037aa2b8ba5627908c2c169de33cbabb90d
kuska1 <kusakaonelove@gmail.com>
2026-06-03 22:23:21 +0300

    matugen option fallback for old folder system (maybe fixed https://github.com/kuska1/Material-Theme/issues/39)

--- js/main/colors/matugen.js
+++ js/main/colors/matugen.js
@@ -1,10 +1,38 @@
 // const fs = require('fs');
 
+const logText = '%c Material '
+const logCss = 'background: #256ab8; color: #ffffff'
+
+console.debug(logText, logCss, `matugen.js is running in ${window.location.href}.`);
+
+const PRIMARY_URL = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
+const FALLBACK_URL = "https://steamloopback.host/skins/Material-Theme/css/main/colors/matugen.css";
+
+let fetch_url = PRIMARY_URL;
+
 const cssLink = document.createElement('link');
 cssLink.rel = 'stylesheet';
-cssLink.href = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
 document.head.appendChild(cssLink);
 
-setInterval(() => {
-  cssLink.href = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
-}, 500); // Check every 5 seconds
+const applyUrl = (url) => {
+  if (cssLink.href !== url) {
+    cssLink.href = url;
+  }
+};
+
+const switchUrl = (reason) => {
+  console.debug(logText, logCss, `${fetch_url} ${reason}, switching...`);
+  fetch_url = fetch_url === PRIMARY_URL ? FALLBACK_URL : PRIMARY_URL;
+};
+
+const checkAndApply = () => {
+  fetch(fetch_url, { method: 'HEAD', cache: 'no-store' })
+    .then(res => {
+      if (!res.ok) switchUrl(`returned ${res.status}`);
+    })
+    .catch(() => switchUrl('unreachable'))
+    .finally(() => applyUrl(fetch_url));
+};
+
+checkAndApply();
+setInterval(checkAndApply, 500); // Check every 5 seconds
