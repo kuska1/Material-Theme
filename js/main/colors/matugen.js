829bdc0bfd35c0c33d58a9e2760abd03afc3b23c
kuska1 <kusakaonelove@gmail.com>
2026-06-03 10:48:16 +0300

    matugen issue fix

--- js/main/colors/matugen.js
+++ js/main/colors/matugen.js
@@ -2,9 +2,9 @@
 
 const cssLink = document.createElement('link');
 cssLink.rel = 'stylesheet';
-cssLink.href = "https://steamloopback.host/skins/Material-Theme/css/main/colors/matugen.css";
+cssLink.href = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
 document.head.appendChild(cssLink);
 
 setInterval(() => {
-  cssLink.href = "https://steamloopback.host/skins/Material-Theme/css/main/colors/matugen.css";
+  cssLink.href = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
 }, 500); // Check every 5 seconds
