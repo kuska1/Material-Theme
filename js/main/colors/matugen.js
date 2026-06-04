8ebb0611da472f5d35df0fde0c172677f9eb995b
kuska1 <kusakaonelove@gmail.com>
2026-06-04 12:24:48 +0300

    matugen fix 2 (i hope)

--- js/main/colors/matugen.js
+++ js/main/colors/matugen.js
@@ -1,38 +1,26 @@
-// const fs = require('fs');
-
 const logText = '%c Material '
 const logCss = 'background: #256ab8; color: #ffffff'
 
 console.debug(logText, logCss, `matugen.js is running in ${window.location.href}.`);
 
-const PRIMARY_URL = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
-const FALLBACK_URL = "https://steamloopback.host/skins/Material-Theme/css/main/colors/matugen.css";
+const FALLBACK_URL = "https://millennium.host/v1/themes/Material-Theme/css/main/colors/matugen.css";
+const PRIMARY_URL = "https://steamloopback.host/skins/Material-Theme/css/main/colors/matugen.css";
 
-let fetch_url = PRIMARY_URL;
+let link_url = PRIMARY_URL;
 
 const cssLink = document.createElement('link');
 cssLink.rel = 'stylesheet';
+
+cssLink.onerror = () => {
+  console.debug(logText, logCss, `Failed to load ${link_url}, switching to another URL...`);
+  link_url === PRIMARY_URL ? link_url = FALLBACK_URL : link_url = PRIMARY_URL;
+  cssLink.href = link_url;
+  cssLink.onerror = null;
+};
+
+cssLink.href = link_url;
 document.head.appendChild(cssLink);
 
-const applyUrl = (url) => {
-  if (cssLink.href !== url) {
-    cssLink.href = url;
-  }
-};
-
-const switchUrl = (reason) => {
-  console.debug(logText, logCss, `${fetch_url} ${reason}, switching...`);
-  fetch_url = fetch_url === PRIMARY_URL ? FALLBACK_URL : PRIMARY_URL;
-};
-
-const checkAndApply = () => {
-  fetch(fetch_url, { method: 'HEAD', cache: 'no-store' })
-    .then(res => {
-      if (!res.ok) switchUrl(`returned ${res.status}`);
-    })
-    .catch(() => switchUrl('unreachable'))
-    .finally(() => applyUrl(fetch_url));
-};
-
-checkAndApply();
-setInterval(checkAndApply, 500); // Check every 5 seconds
+setInterval(() => {
+  cssLink.href = link_url;
+}, 1500);
